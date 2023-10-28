2d617b363897b1dfe621ada70d9d679d6fd8fc80
Amartuvshin Enkhbayar <amaraaready@gmail.com>
2023-10-28 17:59:48 +0800

    fff

--- lib/screens/scale/scale_page.dart
+++ lib/screens/scale/scale_page.dart
@@ -8,7 +8,7 @@ import 'package:libserialport/libserialport.dart';
 import 'package:after_layout/after_layout.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:etos_scale_windows/widgets/form_textfield.dart';
-import 'package:video_player/video_player.dart';
+import 'package:flutter_vlc_player/flutter_vlc_player.dart';
 
 class ScalePage extends StatefulWidget {
   static const routeName = 'ScalePage';
@@ -24,68 +24,60 @@ class _ScalePageState extends State<ScalePage> with AfterLayoutMixin {
   var ports = <String>[];
   String scaleData = "000000";
   GlobalKey<FormBuilderState> fbKey = GlobalKey<FormBuilderState>();
-  late VideoPlayerController _videoPlayerController;
-  bool startedPlaying = false;
+
+  late VlcPlayerController _controller;
 
   @override
   void initState() {
     super.initState();
-
-    _videoPlayerController =
-        VideoPlayerController.asset('assets/Butterfly-209.mp4');
-    _videoPlayerController.addListener(() {
-      if (startedPlaying && !_videoPlayerController.value.isPlaying) {
-        Navigator.pop(context);
-      }
-    });
+    _controller = VlcPlayerController.network(
+      "rtsp://admin:pi@123456@192.168.1.246:554",
+      hwAcc: HwAcc.full,
+      autoPlay: false,
+      options: VlcPlayerOptions(),
+    );
   }
 
   @override
-  void dispose() {
-    _videoPlayerController.dispose();
+  void dispose() async {
+    await _controller.stopRendererScanning();
+    await _controller.dispose();
     super.dispose();
-  }
-
-  Future<bool> started() async {
-    await _videoPlayerController.initialize();
-    await _videoPlayerController.play();
-    startedPlaying = true;
-    return true;
   }
 
   @override
   afterFirstLayout(BuildContext context) {
-    final name = SerialPort.availablePorts.first;
-    final port = SerialPort(name);
-    if (!port.openReadWrite()) {
-      if (kDebugMode) {
-        print(SerialPort.lastError);
-      }
-    }
+    // final name = SerialPort.availablePorts.first;
+    // final port = SerialPort(name);
+    // if (!port.openReadWrite()) {
+    //   if (kDebugMode) {
+    //     print(SerialPort.lastError);
+    //   }
+    // }
 
-    var portConfig = SerialPortConfig()
-      ..baudRate = 9600
-      ..bits = 8
-      ..stopBits = 1;
-    port.config = portConfig;
+    // var portConfig = SerialPortConfig()
+    //   ..baudRate = 9600
+    //   ..bits = 8
+    //   ..stopBits = 1;
+    // port.config = portConfig;
 
-    final reader = SerialPortReader(port);
+    // final reader = SerialPortReader(port);
 
-    var received = "";
+    // var received = "";
 
-    reader.stream.listen((data) {
-      var chr = String.fromCharCodes(data);
+    // reader.stream.listen((data) {
+    //   var chr = String.fromCharCodes(data);
 
-      received += chr;
+    //   received += chr;
 
-      if (received.length == 12) {
-        setState(() {
-          scaleData = received.substring(2, 8);
-        });
+    //   if (received.length == 12) {
+    //     setState(() {
+    //       scaleData = received.substring(2, 8);
+    //     });
 
-        received = "";
-      }
-    });
+    //     received = "";
+    //   }
+    // });
   }
 
   @override
@@ -100,121 +92,117 @@ class _ScalePageState extends State<ScalePage> with AfterLayoutMixin {
               color: backgroundColor,
               child: Column(
                 children: [
-                  FormBuilder(
-                    key: fbKey,
-                    child: const Column(
-                      children: [
-                        Row(
-                          children: [
-                            Expanded(
-                              flex: 3,
-                              child: FormTextField(
-                                textColor: white,
-                                labelText: "Гэрээний дугаар",
-                                inputType: TextInputType.text,
-                                name: 'contractnumber',
-                                hintText: '000000000',
-                                color: lightgrey,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Expanded(
-                              flex: 3,
-                              child: FormTextField(
-                                textColor: white,
-                                labelText: "Пүүний баримт",
-                                inputType: TextInputType.text,
-                                name: 'document',
-                                hintText: '000000000',
-                                color: lightgrey,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Expanded(
-                              flex: 3,
-                              child: SizedBox(),
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 15,
-                        ),
-                        Row(
-                          children: [
-                            Expanded(
-                              flex: 3,
-                              child: FormTextField(
-                                textColor: white,
-                                labelText: "Худалдаалагч",
-                                inputType: TextInputType.text,
-                                name: 'trader',
-                                hintText: '000000000',
-                                color: lightgrey,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Expanded(
-                              flex: 3,
-                              child: FormTextField(
-                                textColor: white,
-                                labelText: "Худалдан авагч",
-                                inputType: TextInputType.text,
-                                name: 'buyer',
-                                hintText: '000000000',
-                                color: lightgrey,
-                              ),
-                            ),
-                            SizedBox(
-                              width: 15,
-                            ),
-                            Expanded(
-                              flex: 3,
-                              child: FormTextField(
-                                textColor: white,
-                                labelText: "Нүүрсний төрөл",
-                                inputType: TextInputType.text,
-                                name: 'number12',
-                                hintText: '000000000',
-                                color: lightgrey,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const CustomContainer(),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  const Trailer(),
-                  const SizedBox(
-                    height: 15,
-                  ),
+                  // FormBuilder(
+                  //   key: fbKey,
+                  //   child: const Column(
+                  //     children: [
+                  //       Row(
+                  //         children: [
+                  //           Expanded(
+                  //             flex: 3,
+                  //             child: FormTextField(
+                  //               textColor: white,
+                  //               labelText: "Гэрээний дугаар",
+                  //               inputType: TextInputType.text,
+                  //               name: 'contractnumber',
+                  //               hintText: '000000000',
+                  //               color: lightgrey,
+                  //             ),
+                  //           ),
+                  //           SizedBox(
+                  //             width: 15,
+                  //           ),
+                  //           Expanded(
+                  //             flex: 3,
+                  //             child: FormTextField(
+                  //               textColor: white,
+                  //               labelText: "Пүүний баримт",
+                  //               inputType: TextInputType.text,
+                  //               name: 'document',
+                  //               hintText: '000000000',
+                  //               color: lightgrey,
+                  //             ),
+                  //           ),
+                  //           SizedBox(
+                  //             width: 15,
+                  //           ),
+                  //           Expanded(
+                  //             flex: 3,
+                  //             child: SizedBox(),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //       SizedBox(
+                  //         height: 15,
+                  //       ),
+                  //       Row(
+                  //         children: [
+                  //           Expanded(
+                  //             flex: 3,
+                  //             child: FormTextField(
+                  //               textColor: white,
+                  //               labelText: "Худалдаалагч",
+                  //               inputType: TextInputType.text,
+                  //               name: 'trader',
+                  //               hintText: '000000000',
+                  //               color: lightgrey,
+                  //             ),
+                  //           ),
+                  //           SizedBox(
+                  //             width: 15,
+                  //           ),
+                  //           Expanded(
+                  //             flex: 3,
+                  //             child: FormTextField(
+                  //               textColor: white,
+                  //               labelText: "Худалдан авагч",
+                  //               inputType: TextInputType.text,
+                  //               name: 'buyer',
+                  //               hintText: '000000000',
+                  //               color: lightgrey,
+                  //             ),
+                  //           ),
+                  //           SizedBox(
+                  //             width: 15,
+                  //           ),
+                  //           Expanded(
+                  //             flex: 3,
+                  //             child: FormTextField(
+                  //               textColor: white,
+                  //               labelText: "Нүүрсний төрөл",
+                  //               inputType: TextInputType.text,
+                  //               name: 'number12',
+                  //               hintText: '000000000',
+                  //               color: lightgrey,
+                  //             ),
+                  //           ),
+                  //         ],
+                  //       ),
+                  //     ],
+                  //   ),
+                  // ),
+                  // const SizedBox(
+                  //   height: 15,
+                  // ),
+                  // const CustomContainer(),
+                  // const SizedBox(
+                  //   height: 15,
+                  // ),
+                  // const Trailer(),
+                  // const SizedBox(
+                  //   height: 15,
+                  // ),
                   // InformationCard(scaleData: scaleData),
-                  FutureBuilder<bool>(
-                    future: started(),
-                    builder:
-                        (BuildContext context, AsyncSnapshot<bool> snapshot) {
-                      if (snapshot.data ?? false) {
-                        return AspectRatio(
-                          aspectRatio: _videoPlayerController.value.aspectRatio,
-                          child: VideoPlayer(_videoPlayerController),
-                        );
-                      } else {
-                        return const Text('waiting for video to load');
-                      }
-                    },
-                  ),
+                  Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        VlcPlayer(
+                          controller: _controller,
+                          aspectRatio: 16 / 9,
+                          placeholder:
+                              const Center(child: CircularProgressIndicator()),
+                        )
+                      ]),
                 ],
               ),
             ),
@@ -224,15 +212,3 @@ class _ScalePageState extends State<ScalePage> with AfterLayoutMixin {
     );
   }
 }
-
-/*
-child: Center(
-              child: Text(
-                receivedData,
-                style: TextStyle(
-                  color: white,
-                  fontSize: 25,
-                  fontWeight: FontWeight.w500,
-                ),
-              ),
-            ),*/
