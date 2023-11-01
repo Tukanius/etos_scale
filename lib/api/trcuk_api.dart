a80133b9421527cbfa07760737ab6ab66f94fb6d
Amartuvshin Enkhbayar <amaraaready@gmail.com>
2023-11-01 11:03:51 +0800

    fff

--- lib/api/trcuk_api.dart
+++ lib/api/trcuk_api.dart
@@ -1,4 +1,4 @@
-import 'package:etos_scale_windows/models/user.dart';
+import 'package:etos_scale_windows/models/receipt.dart';
 import 'package:etos_scale_windows/utils/http_request.dart';
 
 final httpRequest = HttpRequest("http://192.168.1.96:30605/api");
@@ -7,6 +7,6 @@ class TruckApi {
   scale(data) async {
     var res = await httpRequest.post('/truck/scale', data: data);
 
-    return User.fromAuthJson(res as Map<String, dynamic>);
+    return Receipt.fromJson(res as Map<String, dynamic>);
   }
 }
